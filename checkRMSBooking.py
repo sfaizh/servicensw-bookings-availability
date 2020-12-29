941645eecc37154777f06f11b2a9135d35be8b9e
faizan <faizanh53@gmail.com>
2020-12-29 05:55:56 -0800

    corrected files

--- checkRMSBooking.py
+++ checkRMSBooking.py
@@ -18,7 +18,7 @@ data_prev = 'Thurs 14/01|12:00 pm'
 print (data_prev)
 
 while True:
-        time.sleep(900)
+        time.sleep(500)
         # Check for time change
         data_next = subprocess.check_output('sudo node ' + location, shell=True).decode("utf-8").rstrip()
         while not data_next:
@@ -49,8 +49,8 @@ while True:
 
         #newTime = oldTime.replace(hour=10, minute=5, second=0, microsecond=0)
 
-        if (newDate.month <= oldDate.month):# check if date month is earlier
-                if (newDate.day < oldDate.day) and (newDate.day > 8):# check if date day is earlier
+        if (newDate.month == oldDate.month):# check if date month is the same
+                if (newDate.day < oldDate.day) and (newDate.day > 8) and (newDate.day <= 14):# check if date day is earlier
                         data_prev = data_next
                         print ("Found earlier DAY!")
                         #print (data_prev) # this should show 7:05 time
@@ -67,7 +67,7 @@ while True:
                         except SMTPException:
                                 print("Something went wrong...")
 
-                elif (newDate.day == oldDate.day) and (newDate.day > 8): # if same day, check if time is earlier
+                elif (newDate.day == oldDate.day) and (newDate.day > 8) and (newDate.day <= 14): # if same day, check if time is earlier
                         #if(((getTime1.split(' ',1)[1] == "am") and (getTime2.split(' ',1)[1] == "am")) or ((getTime1.split(' ',1)[1] == "pm") and (getTime2.split(' ',1)[1] == "pm"))): # check if both am or pm
                         if(newTime < oldTime): # 7:05 < 8:05
                                 #oldTime = newTime
@@ -91,43 +91,29 @@ while True:
                         #elif (((getTime1.split(' ',1)[1] == "am") and (getTime2.split(' ',1)[1] == "pm")) or ((getTime1.split(' ',1)[1] == "pm") and (getTime2.split(' ',1)[1] == "am"))): # not both am or pm
                                 # do required subtractions
                 else:
-                        #print ("Latest date taken - Rollback day to earliest available")
+                        print ("Latest date taken - Rollback day to earliest available")
                         data_prev = data_next
                         oldTime = newTime
-                        #try:
-                        #        server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-                        #        server_ssl.ehlo()
-                        #        server_ssl.login("fsocietypriv@gmail.com", "Faizan360")
-                        #        SUBJECT = "RMS TIME ROLLBACK " + data_next
-                        #        msg = "Subject: {}\n\n{}".format(SUBJECT, "Latest date taken - Rollback day to earliest available: " + data_next)
-                        #        
-                        #        server_ssl.sendmail("fsocietypriv@gmail.com", "faizanh53@gmail.com", msg)
-                        #        print("Successfully sent rollback email!")
-                        #        #earlierAvailable = true;
-                        #        #time.sleep(1800)
-                        #except SMTPException:
-                        #        print("Something went wrong...")
-                        
 			# Could be that the latest time has been taken, so we need to rollback to the last available time
                         # This use case only happens if the newest date retrieved has been filled and we are left with a later date.
                         #print ("No earlier date available since last check")
+        elif (newDate.month > oldDate.month):# new year special case
+                print ("Found earlier DAY!")
+                data_prev = data_next
+                try:
+                        server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                        server_ssl.ehlo()
+                        server_ssl.login("fsocietypriv@gmail.com", "Faizan360")
+                        SUBJECT = "NEW RMS DAY " + data_next
+                        msg = "Subject: {}\n\n{}".format(SUBJECT, SUBJECT, "Earlier day available: " + data_next)
+
+                        server_ssl.sendmail("fsocietypriv@gmail.com", "faizanh53@gmail.com", msg)
+                        print("Successfully sent email!")
+                        #time.sleep(1800)
+                except SMTPException:
+                        print("Something went wrong...")
         else:
-                # This use case only happens if the newest date retrieved has been filled and we are left with a later date.
-                
-		#print ("Latest date taken - Rollback month to earliest available")
+                 # This use case only happens if the newest date retrieved has been filled and we are left with a later date.
+                print ("Latest date taken - Rollback month to earliest available")
                 data_prev = data_next
                 oldTime = newTime
-                #try:
-                #        server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-                #        server_ssl.ehlo()
-                #        server_ssl.login("fsocietypriv@gmail.com", "Faizan360")
-                #        SUBJECT = "RMS TIME ROLLBACK " + data_next
-                #        msg = "Subject: {}\n\n{}".format(SUBJECT, "Latest date taken - Rollback month to earliest available: " + data_next)
-                #        
-                #        server_ssl.sendmail("fsocietypriv@gmail.com", "faizanh53@gmail.com", msg)
-                #        print("Successfully sent rollback email!")
-                        #earlierAvailable = true;
-                        #time.sleep(1800)
-                #except SMTPException:
-                #        print("Something went wrong...")
-                #print ("No earlier date available since last check")
